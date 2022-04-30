31b07f4429f5082761ec3400b7658da1c1932a28
Higor Silva <higor.junio@hotmail.com>
2022-04-29 22:14:57 -0300

    refactor: move all signup protcols imports to a single file

--- src/presentation/controllers/signup/signUp.ts
+++ src/presentation/controllers/signup/signUp.ts
@@ -1,7 +1,7 @@
-import { HttpRequest, HttpResponse, EmailValidator, Controller } from '../protocols'
-import { badRequest, serverError } from '../helpers/http-helper'
-import { MissingParamError, InvalidParamError } from '../errors'
-import { AddAccount } from '../../domain/usecases/add-account'
+import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount } from './signup-protocols'
+import { badRequest, serverError } from '../../helpers/http-helper'
+import { MissingParamError, InvalidParamError } from '../../errors'
+
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
