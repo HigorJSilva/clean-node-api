802c90cef86de2dd3b9610d06bfc27ed5f36b102
Higor Silva <higor.junio@hotmail.com>
2022-05-05 14:08:12 -0300

    feat: add ExpressRouteAdapter

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,11 +1,10 @@
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
-import env from '../config/env'
 
 describe('SignUp routes', () => {
   beforeAll(async () => {
-    await MongoHelper.connect(env.mongoUrl)
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
   })
 
   afterAll(async () => {
