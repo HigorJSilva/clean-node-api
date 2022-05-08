4cf935302061c84199cf1a3bd5f04400f12f7611
Higor Silva <higor.junio@hotmail.com>
2022-05-08 11:10:16 -0300

    feat: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -30,7 +30,7 @@ export class LoginController implements Controller {
         return unauthorized()
       }
 
-      return ok('email')
+      return ok({ accessToken })
     } catch (error) {
       return serverError(new Error())
     }
