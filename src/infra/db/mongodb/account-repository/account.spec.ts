c9d7b637ed8e45bf02503a87bd5b9aec65265f47
Higor Silva <higor.junio@hotmail.com>
2022-05-18 11:47:42 -0300

    test: ensure AccountMongoRepository updates account accessToken on updateAccesToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,19 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeFalsy()
   })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const result = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const fakeAccountId = result.insertedId
+
+    await sut.updateAccessToken(fakeAccountId.toString(), 'any_token')
+    const account = await accountCollection.findOne({ _id: fakeAccountId })
+    expect(account).toBeTruthy()
+    expect(account?.accessToken).toBe('any_token')
+  })
 })
