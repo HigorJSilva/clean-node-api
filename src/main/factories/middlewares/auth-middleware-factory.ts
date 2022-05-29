168665a19654985e46876d2080022a8857244678
Higor Silva <higor.junio@hotmail.com>
2022-05-29 18:58:09 -0300

    refactor: optional dependency

--- src/main/factories/middlewares/auth-middleware-factory.ts
+++ src/main/factories/middlewares/auth-middleware-factory.ts
@@ -3,5 +3,5 @@ import { Middleware } from '../../../presentation/protocols'
 import { makeDbLoadAccountByToken } from '../usecases/account/load-account-by-token/db-load-account-by-token-factory'
 
 export const makeAuthMiddleware = (role?: string): Middleware => {
-  return new AuthMiddleware(makeDbLoadAccountByToken(), role ?? '')
+  return new AuthMiddleware(makeDbLoadAccountByToken(), role)
 }
