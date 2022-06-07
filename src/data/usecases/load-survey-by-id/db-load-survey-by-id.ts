8dbceb375a1e7db51fca6e51cb96b01f161cb083
Higor Silva <higor.junio@hotmail.com>
2022-06-06 21:44:49 -0300

    feat: ensure DbLoadSurveyById calls repository witch correct id

---
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.ts
@@ -0,0 +1,11 @@
+import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
+import { SurveyModel } from '@/domain/models/survey'
+
+export class DbLoadSurveyById implements DbLoadSurveyById {
+  constructor (private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository) {}
+
+  async loadById (id: string): Promise<SurveyModel> {
+    const survey = await this.loadSurveyByIdRepository.loadById(id)
+    return survey
+  }
+}
