fcc5571243a05d5ba0ce00851301128b531c5bd4
Higor Silva <higor.junio@hotmail.com>
2022-06-09 21:55:00 -0300

    refactor: organize project usecases in folders

--- src/main/factories/usecases/survey/load-surveys/db-add-survey-factory .ts
+++ src/main/factories/usecases/survey/load-surveys/db-add-survey-factory .ts
@@ -1,5 +1,5 @@
-import { DbLoadSurveys } from '@/data/usecases/load-serveys/db-load-surveys'
-import { LoadSurveys } from '@/domain/usecases/load-surveys'
+import { DbLoadSurveys } from '@/data/usecases/survey/load-serveys/db-load-surveys'
+import { LoadSurveys } from '@/domain/usecases/survey/load-surveys'
 import { SurveyMongoRepository } from '@/infra/db/mongodb/survey/survey-mongo-respository'
 
 export const makeDbLoadSurveys = (): LoadSurveys => {
