3762b0830d5868070942ce67e56448b08c6abecd
Higor Silva <higor.junio@hotmail.com>
2022-06-23 21:36:08 -0300

    test: ensure load survey route returns 403 if no token is provided

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -77,4 +77,12 @@ describe('Survey routes', () => {
         .expect(200)
     })
   })
+
+  describe('GET /surveys/:surveyId/results', () => {
+    test('Should return 403 if no accessToken is provided', async () => {
+      await request(app)
+        .get('/api/surveys/any_id/results')
+        .expect(403)
+    })
+  })
 })
