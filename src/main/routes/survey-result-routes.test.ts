c55d2a0b67109db9efb9fd80481c83870cf924d8
Higor Silva <higor.junio@hotmail.com>
2022-06-23 21:38:58 -0300

    test: ensure load survey route returns 200 if token is provided

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -85,4 +85,23 @@ describe('Survey routes', () => {
         .expect(403)
     })
   })
+
+  test('Should return 200 on loadd survey result with accessToken', async () => {
+    const accessToken = await makeAccessToken()
+    const res = await surveyCollection.insertOne({
+      question: 'Question',
+      answers: [{
+        answer: 'Answer 1',
+        image: 'http://image-name.com'
+      }, {
+        answer: 'Answer 2'
+      }],
+      date: new Date()
+    })
+
+    await request(app)
+      .get(`/api/surveys/${res.insertedId}/results`)
+      .set('x-access-token', accessToken)
+      .expect(200)
+  })
 })
