4bf6a7d14aa27fda446557951262a0cb0c7364a2
Higor Silva <higor.junio@hotmail.com>
2022-06-23 21:08:21 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyResult with correct value

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller-protocols.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller-protocols.ts
@@ -1,3 +1,4 @@
 export * from '@/domain/usecases/survey/load-survey-by-id'
 export * from '@/presentation/protocols'
 export * from '@/presentation/test'
+export * from '@/presentation/helpers/http/http-helper'
--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,10 +1,11 @@
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper'
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './load-survey-result-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById, forbidden, ok, serverError } from './load-survey-result-controller-protocols'
 
 export class LoadSurveyResultController implements Controller {
   constructor (
-    private readonly loadSurveyById: LoadSurveyById
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly loadSurveyResult: LoadSurveyResult
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -13,8 +14,9 @@ export class LoadSurveyResultController implements Controller {
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'))
       }
+      const surveyResult = await this.loadSurveyResult.load(httpRequest.params.surveyId)
 
-      return ok({ ok: true })
+      return ok({ surveyResult })
     } catch (error) {
       return serverError(error as Error)
     }
